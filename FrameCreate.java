51ee7c140f7ab29332d98f16a10eecc08325d074
DarshanMistari <mistaridarshan2@gmail.com>
2024-06-05 10:06:48 +0530

    [ADDED]:Darshan Java Code to Create a Simple Frame in Awt

---
+++ FrameCreate.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+class FrameCreate
+{
+	public static void main(String args[])
+	{
+		Frame  f = new Frame();
+
+		f.setSize(400,400);
+		f.setLocation(50,50);
+		f.setTitle("Create Frame");
+		f.setVisible(true);
+	}
+}
